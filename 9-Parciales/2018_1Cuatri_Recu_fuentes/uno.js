f79fd70c28ca0e2a4f8e8220af047474701b3d23
Gianfranco <40763595+giandb99@users.noreply.github.com>
2018-07-04 12:22:13 -0300

    1 y 2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
 
+	base=prompt("Ingrese la base del triangulo");
+	altura=prompt("Ingrese la altura del triangulo");
+	superficie=(base*altura)/2;
+	perimetro=base*3;
+
+	alert("La superficie del triangulo equilatero es " + superficie + " y el perimetro es " + perimetro);	
 }
