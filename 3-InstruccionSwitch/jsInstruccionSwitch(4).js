e06e692f78ebc9a1696d5f1590971f257997423c
Gianfranco <40763595+giandb99@users.noreply.github.com>
2018-07-23 12:27:09 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,10 +1,35 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
+	//alert (mesDelAño);
+		
+	alert(mesDelAño);
+
+	switch(mesDelAño)
+		{
+			case "Enero":
+			case "Marzo":
+			case "Mayo":
+			case "Julio":
+			case "Agosto":
+			case "Octubre":
+			case "Diciembre":
+				alert("Este mes tiene 31 días.");
+				break;
+
+			case "Febrero":
+			 	alert("Este mes tiene 28 días.");
+			 	break;
+
+			case "Abril":
+			case "Junio":
+			case "Septiembre":
+			case "Noviembre":
+				alert("Este mes tiene 30 días");
+				break;
+		}
 	
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,10 +1,22 @@
 function mostrar()
 {
-//tomo la edad  
-var laHora = document.getElementById('hora').value;
+	//tomo la edad  
+	var laHora = document.getElementById('hora').value;
 
-//alert (laHora);
+	//alert (laHora);
 	
+	alert (laHora);
+
+	switch(laHora)
+		{
+			case "7":
+			case "8":
+			case "9":
+			case "10":
+			case "11":
+				alert("Es de mañana");
+				break;
+		}	
 	
 
 
