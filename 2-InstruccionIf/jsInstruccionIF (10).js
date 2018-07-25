7a991c9adef1493f0983ec5db3ef5019ee589c75
Gianfranco <40763595+giandb99@users.noreply.github.com>
2018-07-25 12:24:40 -0300

    9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var nota=Math.floor((Math.random()*10)+1);
 	
+	alert(nota);
+
+	if(nota == 9 || nota==10)
+	{
+		alert("Excelente.");
+	}
+	else if (nota>=4)
+		{
+			alert("Aprobó.");
+		}
+		else
+		{
+			alert("Vamos, la próxima se puede.");
+		}
 
 }//FIN DE LA FUNCIÓN
