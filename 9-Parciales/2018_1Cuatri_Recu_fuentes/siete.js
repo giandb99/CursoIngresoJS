a6e9644df8ab3769b63731417b6a28918bb9c855
Gianfranco <40763595+giandb99@users.noreply.github.com>
2018-08-01 12:20:57 -0300

    ejercicio 7

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -20,55 +20,86 @@ Mostrar:
 function mostrar()
 {
 	var nombre;
-	var edad;
 	var sexo;
-	var contador;
-	var contadorMayoresEdad;
-	var contadorMenoresEdad;
-
-	contador=0;
-	contadorHombres=0;
-	contadorMujeres=0;
-	contadorMayoresEdad=0;
-	contadorMenoresEdad=0;
+	var edad;
+	var contadorMayoresEdad=0;
+	var contadorMenoresEdad=0;
+	var contadorHombres=0;
+	var contadorMujeres=0;
+	var nombreMayor;
+	var nombreMenor;
+	var nombreHombreMenor;
+	var mayorEdad;
+	var menorEdad;
+	var contador=0;
+	var edadHombreMenor;
+	var flag=0;
+	
 
 	while(contador < 3)
 	{
 		contador++;
 		nombre=prompt("Ingrese el nombre");
 		sexo=prompt("Ingrese el sexo");
-		while(sexo!='f' && sexo !='m')
+		edad=parseInt(prompt("Ingrese la edad"));
+		
+		if (edad < 18)
 		{
-			sexo=prompt("Error. Reingrese el sexo");
-			if(sexo=='f')
-			{
-				contadorMujeres++;
-			}
-			else
-			{
-				contadorHombres++;
-			}
+			contadorMenoresEdad++;
+		}
+		else
+		{
+			contadorMayoresEdad++;
 		}
 		
-		edad=parseInt(prompt("Ingrese la edad"));
-		while(edad < 0 || edad > 100)
+		if (sexo == 'm')
 		{
-			edad=parseInt(prompt("Error. Reingrese la edad"));
-			if(edad > 18)
-			{
-				contadorMayoresEdad++;
-			}
-			else
-			{
-				contadorMenoresEdad++;
-			}
+			contadorHombres++;
+		}
+		else
+		{
+			contadorMujeres++;
+		}
+
+		if (edad > mayorEdad || flag==0)
+		{
+			mayorEdad = edad;
+			nombreMayor = nombre;
+		}
+
+		if (edad < menorEdad || flag==0)
+		{
+			menorEdad=edad;
+			nombreMenor=nombre;
+			flag=1;
+		}
+		
+		if (sexo == 'm' && contadorHombres == 1)
+		{
+			nombreHombreMenor = nombre;
+			edadHombreMenor = edad;
+		}
+
+		if (sexo == 'm' && edad < edadHombreMenor)
+		{
+			nombreHombreMenor = nombre;
+			edadHombreMenor = edad;
 		}
 	}
+	
+	document.write("La cantidad de mayores de edad es: " + contadorMayoresEdad + "<br>" );
+	document.write("La cantidad de menores de edad es: " + contadorMenoresEdad + "<br>");
+	document.write("La cantidad de mujeres es: " + contadorMujeres + "<br>");
+	document.write("La cantidad de hombres es: " + contadorHombres + "<br>");
+	document.write("El nombre de la persona de mayor edad es: " + nombreMayor + "<br>");
+	document.write("El nombre de la persona de menor edad es: " + nombreMenor + "<br>");
 
-	document.write("<br>  La cantidad de mayores de edad es: "+cantidadMayoresEdad);
-	document.write("<br>  La cantidad de menores de edad es: "+cantidadMenoresEdad);
-	document.write("<br>  La cantidad de mujeres es: "+cantidadMujeres);
-	document.write("<br>  La cantidad de hombres es: "+cantidadHombres);
+	if(contadorHombres==0)
+	{
+		nombreHombreMenor="No se ingreso ningun hombre";
+	}
+
+	document.write("El nombre del hombre de menor edad es: " + nombreHombreMenor + "<br>");
 }
 
 
